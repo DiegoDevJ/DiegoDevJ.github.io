786edf76008851908e0a6c97aa7b64fa16ccd296
DiegoDevJ <145824101+DiegoDevJ@users.noreply.github.com>
2024-06-11 22:56:05 -0300

    Upload do site namorados

---
+++ script.js
@@ -0,0 +1,31 @@
+function checkAnswers() {
+    const correctAnswers = {
+        q1: "15/04/2024",
+        q2: "01/06/2024",
+        q3: "31/01/2006"
+    };
+
+    const userAnswers = {
+        q1: document.getElementById('q1').value,
+        q2: document.getElementById('q2').value,
+        q3: document.getElementById('q3').value
+    };
+
+    let allCorrect = true;
+
+    for (let key in correctAnswers) {
+        if (correctAnswers[key] !== userAnswers[key]) {
+            allCorrect = false;
+            break;
+        }
+    }
+
+    const resultElement = document.getElementById('result');
+    if (allCorrect) {
+        resultElement.innerHTML = 'Aeeeeeeee  <a href="file:///C:/Users/mateu/Downloads/Namorados/Te%20amoo/index.html#">Clique aqui para ir ao site final</a>.';
+        resultElement.style.color = 'green';
+    } else {
+        resultElement.innerHTML = 'Errou 😒 Bora tentar mais uma vez';
+        resultElement.style.color = 'red';
+    }
+}
